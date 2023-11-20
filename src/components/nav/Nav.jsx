7b99b0afb98ab2ae91d14ba1db44febbb8bb5e4c
yukidayo31 <faizahsiti888@gmail.com>
2023-11-21 06:30:57 +0700

    docs: nav component has been added

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./nav.css";
+import { IoHome } from "react-icons/io5";
+import { FaUser } from "react-icons/fa";
+import { FaBook } from "react-icons/fa";
+import { RiServiceLine } from "react-icons/ri";
+import { BiMessageSquareDetail } from "react-icons/bi";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <a href="#" className="active">
+        <IoHome />
+      </a>
+      <a href="#about">
+        <FaUser />
+      </a>
+      <a href="#experience">
+        <FaBook />
+      </a>
+      <a href="#services">
+        <RiServiceLine />
+      </a>
+      <a href="#contact">
+        <BiMessageSquareDetail />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
